086decd40c4ed4a2d61a23304f5eb62a9d5a3b07
ray sonntag <raysonntag@yahoo.com>
2019-10-14 21:51:03 -0400

    junk3 start

---
+++ junk3.js
@@ -0,0 +1,26 @@
+ var i;var t="";var r;var u;var ii;var rr;var rand;
+ 
+ 
+ function funk() {
+    for(i=0;i<11;i++);{
+
+document.getElementById("bo1").innerHTML=i;
+}
+ }
+ 
+ function funk1() {
+document.getElementById("bo2").innerHTML=i;
+}
+t++
+function funk2() {
+ 
+document.body.style.backgroundColor="rgb(230,205,t)";
+
+for (i=0;i<10;i++ ) {
+document.getElementById("bo4").innerHTML=t;
+
+}
+
+
+}
+ 
