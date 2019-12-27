011c3ee8e8839de0c0104d8a53b000bbd282c222
ray sonntag <raysonntag@yahoo.com>
2019-12-27 18:56:52 -0500

    color fade in

--- junk3.js
+++ junk3.js
@@ -1,39 +1,19 @@
- var a,b,c,d,e,f,i,n,r,s,t,aray=[],may=[],ray=[],say=[];
- b01=document.getElementById("bo1");
- bo2=document.getElementById("bo2");
- bo3=document.getElementById("bo3");
- bo4=document.getElementById("bo4");
- 
- bu1.addEventListener("click",fun1);
- bu2.addEventListener("click",fun2);
- bu3.addEventListener("click",fun3);
- bu4.addEventListener("click",fun4);
+  var a,b,c,d,e,m,n,o,r,s,t,arry=[],may=[],ray=[],say=[];
 
 
-
- function fun1() {
-    for(i=0;i<10;i++) {
-    var rand=Math.floor(Math.random()*100);
-      aray.push(rand);
-   if(rand%2==0) {
-    may.push(rand);
-   }
-   else {
-      ray.push(rand);
-   }
-   }
-      bo1.innerHTML=aray;
+bo1.onclick=function() {
+      var rand=Math.floor(Math.random() *100);
+    bo1.innerHTML=rand;
 }
-   function fun2() {
-      bo4.innerHTML=may;
-
-   }
-   function fun3() {
-      bo5.innerHTML=ray;
-   }
-   function fun4() {
-      bo1.innerHTML="";
-      bo2.innerHTML="";
-      bo4.innerHTML="";
-      bo5.innerHTML="";
-   }
+bo2.onclick=function() {
+   var rand=Math.floor(Math.random() *100);
+   bo2.innerHTML=rand;
+}
+bo3.onclick=function() {
+   var rand=Math.floor(Math.random() *100);
+   bo3.innerHTML=rand;
+}
+bo4.onclick=function() {
+   var rand=Math.floor(Math.random() *100);
+   bo4.innerHTML=rand;
+}
