292713188f74060c9d553f35856e41291735ef69
ray sonntag <raysonntag@yahoo.com>
2020-01-16 22:00:07 -0500

    update

--- junk3.js
+++ junk3.js
@@ -1,25 +1,22 @@
  var a,b,c,d,e,m,n,o,arry=[],may=[],ray=[],say=[];
-
  bo1=document.getElementById("bo1");
  bo2=document.getElementById("bo2");
+ bo3=document.getElementById("bo3");
+ bo4=document.getElementById("bo4");
 
- bu1.addEventListener("click",fun1);
- 
+ bo1.addEventListener("click",fun1);
+
  function fun1() {
-        rand={
-          ran1:Math.floor(Math.random() * 10),
-          ran2:Math.floor(Math.random() * 10),
-          ran3:Math.floor(Math.random() * 10),
-          ran4:Math.floor(Math.random() *10)
+    for (i=0;i<100;i++) {
+      arry.push(i);
+       if(i%3==0) {
+         may.push(i);
+      }
+       if(i%9==0) {
+         ray.push(i);
+       }
 
-      };
-      arry.push(rand.ran1, rand.ran2,rand.ran3,rand.ran4);
-      bo1.innerHTML=rand.ran1;
-      bo2.innerHTML=rand.ran2;
-      bo3.innerHTML=rand.ran3;
-      bo4.innerHTML=rand.ran4;
-      bo5.innerHTML=arry;
    }
-   bu2.onclick=function() {
-      bo7.innerHTML=arry;
-   }
+   bo1.innerHTML=may.join(" ");
+   bo2.innerHTML=ray.join(" ");
+ }
