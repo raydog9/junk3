0c6a0bae3f96ac00c037366328fc61af5b06446f
ray sonntag <raysonntag@yahoo.com>
2020-01-17 19:00:01 -0500

    update

--- junk3.js
+++ junk3.js
@@ -1,22 +1,38 @@
- var a,b,c,d,e,m,n,o,arry=[],may=[],ray=[],say=[];
+ var a,b,c,d,e,m,n,r,s,t,arry=[],may=[],ray=[],say=[];
  bo1=document.getElementById("bo1");
  bo2=document.getElementById("bo2");
  bo3=document.getElementById("bo3");
  bo4=document.getElementById("bo4");
 
  bo1.addEventListener("click",fun1);
+ bo5.addEventListener("click",fun2);
 
  function fun1() {
-    for (i=0;i<100;i++) {
-      arry.push(i);
-       if(i%3==0) {
-         may.push(i);
-      }
-       if(i%9==0) {
-         ray.push(i);
-       }
+    
 
-   }
-   bo1.innerHTML=may.join(" ");
-   bo2.innerHTML=ray.join(" ");
+  rand= {
+    ran1:Math.floor(Math.random() *20),
+    ran2:Math.floor(Math.random() *20)+20,
+    ran3:Math.floor(Math.random() *20)+40,
+    ran4:Math.floor(Math.random() *20)+60
+  };
+    arry.push(rand.ran1);
+    ray.push(rand.ran2);
+    may.push(rand.ran3);
+    say.push(rand.ran4);
+
+    bo1.innerHTML=rand.ran1;
+    bo2.innerHTML=rand.ran2;
+    bo3.innerHTML=rand.ran3;
+    bo4.innerHTML=rand.ran4;
+  
+
+
+ }
+ function fun2() {
+  bo5.innerHTML=arry.join(" ");
+  bo6.innerHTML=ray.join(" ");
+  bo7.innerHTML=may.join( " ");
+  bo8.innerHTML=say.join(" ");
+
  }
